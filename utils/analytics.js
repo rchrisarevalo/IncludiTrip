2f19d1e87d6be6ca1aeb66b6d2482c62b2e940b7
Mauro Castillo III <mauro.m.castillo3@gmail.com>
2024-09-02 21:41:14 -0500

    Added Google Analytics

---
+++ utils/analytics.js
@@ -0,0 +1,9 @@
+import ReactGA from "react-ga";
+
+export const initGA = () => {
+  ReactGA.initialize("G-4BND2DZFCF");
+};
+
+export const logPageView = () => {
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+};
