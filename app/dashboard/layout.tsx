47b0370eb502dacd5dfbb738a32c8620bf27f28c
rchrisarevalo <rubenplayer105@gmail.com>
2024-09-13 20:27:07 -0500

    Stashing current changes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ProtectedRoute } from "../components/UserSession";
+import AuthNav from "../components/AuthNav";
 
 interface DashboardLayoutProps {
     children: React.ReactNode;
@@ -9,6 +10,7 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
     return (
         <ProtectedRoute>
             <div className="min-h-screen bg-[#23465d] flex flex-col items-center font-['Poppins'] justify-center gap-10">
+                <AuthNav />
                 { children }
             </div>
         </ProtectedRoute>
